dff0eac4a70eaa3aa4ece7077efa063f3c8e0dd4
kazuyasi <kazuya.u@gmail.com>
2017-03-08 01:07:09 +0900

    バグだらけですがコミット

--- ked/Program.cs
+++ ked/Program.cs
@@ -11,8 +11,214 @@ using Sprache;
 
 namespace ked
 {
+    class Script
+    {
+        public Address Address;
+        public Command Command;
+
+        public Script(Address a, Command cmd)
+        {
+            this.Address = a;
+            this.Command = cmd;
+        }
+
+        public string ExtractCommand(string script)
+        {
+            return script.Substring(this.Address.ToString().Length);
+        }
+    }
+
+    class Address
+    {
+        public string Start { private set; get; }
+        public OptionAddress Option { private set; get; }
+        public bool Not { private set; get; }
+
+        public Address(string st)
+        {
+            this.Start = st;
+            this.Option = new OptionAddress();
+            this.Not = false;
+        }
+
+        public Address(string st, char n)
+        {
+            this.Start = st;
+            this.Option = new OptionAddress();
+            this.Not = (n == '!');
+        }
+
+        public Address(string st, OptionAddress oa, string n)
+        {
+            this.Start = st;
+            this.Option = oa;
+            this.Not = (n == "!");
+        }
+
+        public Address(string st, char v, string o, char n)
+        {
+            this.Start = st;
+            this.Option = new OptionAddress(v, o);
+            this.Not = (n == '!');
+        }
+
+        public override string ToString()
+        {
+            return this.Start + this.Option.Verb +  this.Option.Object + (this.Not? "!": "");
+        }
+
+        public string ExtractCommand(string script)
+        {
+            return script.Substring(this.ToString().Length);
+        }
+
+        public int[] GetIndex(List<string> input)
+        {
+            List<int> ret = new List<int>();
+            int end = input.Count - 1;
+
+            if (Start == "$") ret.Add(end);
+            if (Start != "$" && Option.Verb == 0) ret.Add(int.Parse(Start));
+            if (Start != "$" && Option.Verb == ',')
+            {
+                int endidx = (Option.Object == "$") ? end : int.Parse(Option.Object);
+
+                for (int i = int.Parse(Start); i <= endidx; i++)
+                {
+                    ret.Add(i);
+                }
+            }
+            if (Start != "$" && !Not && Option.Verb == '~')
+            {
+                if (Option.Object == "$") throw new ArgumentException();
+
+
+                int step = int.Parse(Option.Object);
+
+                for(int i = int.Parse(Start); i <= end; i += step)
+                {
+                    ret.Add(i);
+                }
+            }
+
+            //Notがあるなら否定で返す。
+            if(Not)
+            {
+                List<int> tmp = new List<int>();
+
+                //全集合
+                for (int i = 0; i <= end; i++)
+                {
+                    tmp.Add(i);
+                }
+
+                //今まで入力した分を削除
+                foreach (int i in ret.ToArray())
+                {
+                    tmp.Remove(i);
+                }
+
+                ret = tmp;
+            }
+
+            return ret.ToArray();
+        }
+    }
+
+    class OptionAddress
+    {
+        public char Verb { private set; get; }
+        public string Object { private set; get; }
+
+        public OptionAddress()
+        {
+
+        }
+
+        public OptionAddress(char v, string o)
+        {
+            this.Verb = v;
+            this.Object = o;
+        }
+    }
+
+    class Command
+    {
+        public char Operation { private set; get; }
+        public string TargetText { private set; get; }
+        public string ReplaceText { private set; get; }
+
+        public Command()
+        {
+
+        }
+
+        public Command(char op)
+        {
+            this.Operation = op;
+        }
+
+        public Command(char op, string text1, string text2)
+        {
+            this.Operation = op;
+            this.TargetText = text1;
+            this.ReplaceText = text2;
+        }
+
+        public override string ToString()
+        {
+            return (this.Operation == 's')? 
+                this.Operation + "/" + this.TargetText + "/" + this.ReplaceText + "/" :
+                this.Operation.ToString();
+        }
+
+        
+    }
+
     class Program
     {
+        static readonly Parser<string> digit = Parse.Digit.AtLeastOnce().Text();
+        static readonly Parser<IEnumerable<string>> digits = digit.Many();
+
+        static readonly Parser<string> Text = Parse.Letter.AtLeastOnce().Text();
+
+        /// <summary>
+        /// Addressとなる数字か＄をパースするパーサ。
+        /// "100$"などは正常にパースしてしまう……。警告できない。
+        /// </summary>
+        static readonly Parser<string> AddressNumber = digit.Or(Parse.String("$").Text());
+
+        static readonly Parser<OptionAddress> OptionAddress =
+            from commma in Parse.Char(',').Or(Parse.Char('~'))
+            from argNum in AddressNumber
+            select new OptionAddress(commma, argNum);
+
+        static readonly Parser<Address> AddressText =
+            from startAddress in AddressNumber
+            from option in OptionAddress.XOr<OptionAddress>(Parse.Return(new OptionAddress()))
+            from not in Parse.String("!").Text().XOr(Parse.Return(""))
+            select new Address(startAddress, option, not);
+
+        /// <summary>
+        /// 引数を取らないコマンドのパーサー。ArgumentCommandとOrを取るとエラー
+        /// </summary>
+        static readonly Parser<Command> NoArgumentCommand =
+            from cmd in Parse.Char('d').Or(Parse.Char('p')).End()
+            select new Command(cmd);
+
+        /// <summary>
+        /// 引数を取るコマンドのパーサー。NoArgumentCommandとOrを取るとエラー
+        /// </summary>
+        static readonly Parser<Command> ArgumentCommand =
+            from cmd in Parse.Char('s')
+            from slash1 in Parse.Char('/')
+            from text1 in Text
+            from slash2 in Parse.Char('/')
+            from text2 in Text.XOr(Parse.Return(""))
+            from slash3 in Parse.Char('/').End()
+            select new Command(cmd, text1, text2);
+
+
         static void Main(string[] args)
         {
             ReadOnlyCollection<string> OPTION_HAS_ARG = Array.AsReadOnly(new string[] { "e" });
@@ -20,6 +226,7 @@ namespace ked
             List<string> option = new List<string>();
             List<string> script = new List<string>();
             List<string> input = new List<string>();
+            List<string> patternSpace = new List<string>();
 
             // args を　入力パス、オプション、スクリプトの3つに分ける。
             if (args.Length != 0)
@@ -70,6 +277,8 @@ namespace ked
                     return;
                 }
 
+                bool silentMode = false;
+
                 // option解釈（普通編:逐次解釈するものはこちらへ）
                 foreach (string op in option.ToArray())
                 {
@@ -78,6 +287,10 @@ namespace ked
                         case "e":
                             string opArg;   // Option argment
                             if (string.IsNullOrEmpty(opArg = GetOptionArg(option, "e"))) enc = Encoding.GetEncoding(opArg);
+                            break;
+
+                        case "n":
+                            silentMode = true;
                             break;
 
                         default:
@@ -94,6 +307,73 @@ namespace ked
                     Console.WriteLine("No input! XD");
                     return;
                 }
+
+                // パターンスペースへコピー,サイレントモードなら空のまま
+                if (!silentMode) patternSpace = input;
+
+                //スクリプトを逐次実行
+                foreach (string st in script.ToArray())
+                {
+                    // 例外が発生するのでAddress→引数なしコマンド→引数有りコマンドの順にパースする
+                    var adresult = AddressText.TryParse(st);
+
+                    if(adresult.WasSuccessful)
+                    {
+                        var ad = adresult.Value;
+
+                        var ct = ad.ExtractCommand(st);
+
+                        var cmdresult = NoArgumentCommand.TryParse(ct);
+                        if (!cmdresult.WasSuccessful) cmdresult = ArgumentCommand.TryParse(ct);
+
+                        if(cmdresult.WasSuccessful)
+                        {
+                            var cmd = cmdresult.Value;
+
+                            Script sc = new Script(ad, cmd);
+
+                            int[] idxs = sc.Address.GetIndex(input);
+                            switch (sc.Command.Operation)
+                            {
+                                case 'd':
+                                    //削除は逆順に行い、順番が乱れないようにする。
+                                    for (int i = 0; i < idxs.Length; i++)
+                                    {
+                                        patternSpace.RemoveAt(idxs[idxs.Length - 1 - i]);
+                                    }
+                                    break;
+
+                                case 'p':
+                                    for (int i = 0; i < idxs.Length; i++)
+                                    {
+                                        patternSpace.Add(input[idxs[i]]);
+                                    }
+                                    break;
+
+                                case 's':
+                                    for (int i = 0; i < idxs.Length; i++)
+                                    {
+                                        patternSpace[i] = patternSpace[i].Replace(sc.Command.TargetText, sc.Command.ReplaceText);
+                                    }
+                                    break;
+
+                                default:
+                                    break;
+                            }
+                        }
+                    }
+                }
+
+                StringBuilder sb = new StringBuilder();
+                foreach (string line in patternSpace.ToArray())
+                {
+                    sb.AppendLine(line);
+                }
+
+                Console.Write(sb);
+#if DEBUG
+                Console.ReadLine();
+#endif
             }
             else
             {
@@ -173,14 +453,7 @@ namespace ked
         /// <param name="enc"></param>
         private static void ExtractInput(List<string> path, List<string> input, Encoding enc)
         {
-            using (TextReader tr = Console.In)
-            {
-                while (tr.Peek() != -1)
-                {
-                    input.Add(tr.ReadLine());
-                }
-            }
-
+            
             // pathが存在するならテキストファイルとして指定エンコードで読み込む。
             if (path.Count != 0)
             {
@@ -192,6 +465,17 @@ namespace ked
                         {
                             input.Add(sr.ReadLine());
                         }
+                    }
+                }
+            }
+            else
+            {
+                using (TextReader tr = Console.In)
+                {
+                    string line;
+                    while ((line = tr.ReadLine()) != null)
+                    {
+                        input.Add(line);
                     }
                 }
             }
